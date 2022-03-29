3bbf9c8b8f145093d05964a5aefee9cdeb8cd3a2
ohjiyeon3275 <ojy940716@gmail.com>
2022-03-29 22:18:32 +0900

    templates added

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Paperbase from './Paperbase';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Paperbase/>
   </React.StrictMode>,
   document.getElementById('root')
 );
