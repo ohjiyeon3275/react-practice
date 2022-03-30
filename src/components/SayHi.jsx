d65d461b81356e94973728022424a7d3ba109922
ohjiyeon3275 <ojy940716@gmail.com>
2022-03-30 21:45:33 +0900

    unused file deleted

--- src/components/SayHi.jsx
+++
@@ -1,9 +0,0 @@
-type GreetingProps = {
-    name: String
-}
-
-export const SayHi = (props: GreetingProps) => {
-    return(
-        <h1>How are ya { props.name }</h1>
-    )
-}
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Paperbase from './Paperbase';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
     <Paperbase/>
   </React.StrictMode>,
   document.getElementById('root')
