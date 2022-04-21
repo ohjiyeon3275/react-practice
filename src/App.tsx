a5ec60cae3ddc615f025c7b05f2dd22cad568d9c
ohjiyeon3275 <ojy940716@gmail.com>
2022-04-21 21:53:17 +0900

    react version updated to 18

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import React, { useState } from 'react';
 import { useQuery } from 'react-query';
 
 //components
-import { Drawer } from '@mui/material';
-import { LinearProgress } from '@mui/material';
-import { Grid } from '@mui/material';
-import { AddShoppingCart, DataObjectOutlined } from '@mui/icons-material';
-import { Badge } from '@mui/material';
+import Drawer from '@material-ui/core/Drawer';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Grid from '@material-ui/core/Grid';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import Badge from '@material-ui/core/Badge';
 
 //styles (styled component)
 import { Wrapper } from './App.styles';
+import Item from './item/Item';
 
 //Types
 export type CartItemType = {
@@ -37,7 +38,7 @@ const App = () => {
 
   const getTotalItems = () => null
 
-  const handleAddToCart = () => null
+  const handleAddToCart = (clickedItem: CartItemType) => null
 
   const handleRemoveFromCart = () => null
 
@@ -46,9 +47,15 @@ const App = () => {
   if (error) return <div> something went wrong :( </div>
 
   return (
-    <div className="App">
-      start
-    </div>
+    <Wrapper>
+      <Grid item container spacing={3}>
+        {data?.map(item => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handleAddToCart={handleAddToCart}/>
+          </Grid>
+        ))}
+        </Grid>
+    </Wrapper>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+
 import App from './App';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 const client = new QueryClient()
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
 
-root.render(
+// const root = ReactDOM.createRoot(
+//   document.getElementById('root') as HTMLElement
+// );
+
+// if (!root) throw new Error('Failed to find the root element');
+
+ReactDOM.render(
 <QueryClientProvider client={client}>
   <App />
-</QueryClientProvider>
-    
+</QueryClientProvider>,
+
+document.getElementById('root') as HTMLElement
+
 );
 
